e2733448db1ae418588c0a876a686cd5b8cebfea
squiffel <qytiswork@gmail.com>
2024-01-13 22:16:37 -0500

    Dialog fixed

--- UserInterface/CharacterDialog/CharacterDialog.gd
+++ UserInterface/CharacterDialog/CharacterDialog.gd
@@ -1,12 +1,16 @@
 extends CenterContainer
 
+signal dialog_finished
+
 @export var char_name : String = "Steven"
 @export var dialog : String = "Hello World!"
-@export var margin_bottom : int = 48
+@export var margin_bottom : int = 56
 @export var word_delay : float = 0.25
 
 @onready var dialog_label : Label = $PanelContainer/MarginContainer/VBoxContainer/DialogLabel
 @onready var name_label : Label = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/NameLabel
+
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
 @onready var button : Button = $PanelContainer/MarginContainer/VBoxContainer/Button
 
 var words_spoken : String = ""
@@ -14,9 +18,11 @@ var words_split : Array = []
 var i : int = 0
 
 func _ready():
+	button.grab_focus()
+	button.text = "Skip"
+	# Word
 	name_label.text = char_name
 	append_words_with_timer()
-	button.text = "Skip"
 	# Responsive code to Window Size changes
 	get_tree().get_root().size_changed.connect(_on_size_changed)
 	call_deferred("_on_size_changed")
@@ -29,6 +35,8 @@ func append_words_with_timer() -> void:
 	get_tree().create_timer(1.0).timeout.connect(_on_timer_timeout)
 
 func _on_timer_timeout() -> void:
+	if dialog_label.text == dialog:
+		return
 	words_spoken += words_split[i] + " "
 	dialog_label.text = words_spoken
 	i += 1
@@ -37,6 +45,18 @@ func _on_timer_timeout() -> void:
 	else:
 		button.text = "Next"
 
-# If the control size changes
 func _on_resized():
 	_on_size_changed()
+
+func _on_button_pressed():
+	if button.text == "Skip":
+		dialog_label.text = dialog
+		button.text = "Next"
+		return
+	animation_player.current_animation = "FadeOut"
+	animation_player.play()
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "FadeOut":
+		emit_signal("dialog_finished")
+		call_deferred("queue_free")
