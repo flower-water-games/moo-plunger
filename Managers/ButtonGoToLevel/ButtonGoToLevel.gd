789f9ff1703b24d0be393166c130df3101868b3d
Carlos Michael <carlosmichaeldev@gmail.com>
2024-01-17 21:44:56 -0500

    trying something moo

--- Managers/ButtonGoToLevel/ButtonGoToLevel.gd
+++ Managers/ButtonGoToLevel/ButtonGoToLevel.gd
@@ -1,10 +1,10 @@
 extends Node
 
 @export var button : Button
-@export var level_path : PackedScene = preload("res://UserInterface/MainMenu/MainMenu.tscn")
+@export var level_path : String = ("res://UserInterface/MainMenu/MainMenu.tscn")
 
 func _ready():
 	button.pressed.connect(_go_to_level)
 
 func _go_to_level():
-	get_tree().change_scene_to_packed(level_path)
+	get_tree().change_scene_to_file(level_path)
