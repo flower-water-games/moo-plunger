a4e8c77ac74cc025b5548f9463571d7aa2137dcc
Carlos Michael <carlosmichaeldev@gmail.com>
2024-01-18 20:14:30 -0500

    added shooting cows, and floating cows

--- Assets/Bokhum/InflatingCowTest/cow.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-# on ready, get animation player node named "AnimationPlayer" and start animation on loop "Idle_Good"
-# save animationplayer as a variable so we can use it later
-@onready var CowAnimationPlayer : AnimationPlayer = get_node("AnimationPlayer")
-
-#func _ready():
-#CowAnimationPlayer.play("Idle_Good")
