a2317856589cbde90f9d08670751714615684500
squiffel <qytiswork@gmail.com>
2024-01-20 10:01:12 -0500

    Adding

--- Entities/PlungerGun/PlungerGun.gd
+++ Entities/PlungerGun/PlungerGun.gd
@@ -1,24 +1,32 @@
 extends Node3D
 
 var plunger_scene = load("res://Entities/Plunger/Plunger.tscn")
-var plunger_attached : CharacterBody3D
-var plunger_detached : CharacterBody3D
+
+@onready var gun_plunger : CharacterBody3D = $Plunger
+var world_plunger : CharacterBody3D
+
+var action_shoot : bool = false
+var action_stuck : bool = false
+var action_retrieve : bool = false
+
+var max_distance_from_player : int = 30
 
 func _ready():
 	# Add the Plunger to Level3D. This version of the Plunger has been shot out of the gun
-	plunger_detached = plunger_scene.instantiate()
-	plunger_detached.global_position = global_position
-	get_tree().get_root().add_child(plunger_detached)
+	world_plunger = plunger_scene.instantiate()
+	world_plunger.global_position = gun_plunger.global_position
+	get_tree().get_root().add_child(world_plunger)
 	
 	# This Plunger is directly attached to the gun so it will have all the same animations
-	
 
 
 func _input(event):
-	#if event is InputEventKey:
-	#	if Input.is_action_pressed("shoot"):
-	#		var plunger 
-	pass		
+	if event is InputEventKey:
+		if Input.is_action_pressed("shoot"):
+			_shoot_plunger()
+
+func _shoot_plunger():
+	pass
 
 func _process(delta):
 	pass
