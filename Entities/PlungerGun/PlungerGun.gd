d9026948a21721e0b11a69355ac84e9cd1731f64
Carlos Michael <carlosmichaeldev@gmail.com>
2024-01-20 23:13:22 -0500

    boilerplate

--- Entities/PlungerGun/PlungerGun.gd
+++ Entities/PlungerGun/PlungerGun.gd
@@ -2,10 +2,15 @@ extends Node3D
 
 var plunger_scene = load("res://Entities/Plunger/Plunger.tscn")
 
+@export_subgroup("Properties")
+@export var movement_speed = 5
+@export var jump_strength = 8
+
+
 @onready var plunger_end : Node3D = $PlungerEnd
 var world_plunger : CharacterBody3D
 
-enum State {SHOOT, STUCK, RETURN, DEFAULT}
+enum State {SHOOT, STUCK, RETURNING, DEFAULT}
 var plunger_state = State.DEFAULT
 
 var max_distance_from_player : int = 30
@@ -24,6 +29,16 @@ func _shoot_plunger():
 		world_plunger.direction = global_position.direction_to(plunger_end.global_position)
 		print("Fire!")
 
+func _return_plunger():
+	if plunger_state == State.SHOOT:
+		plunger_state = State.RETURNING
+		world_plunger.speed = 10.0
+		world_plunger.direction = world_plunger.global_position.direction_to(plunger_end.global_position)
+		print("Return!")
+
+
+
+
 func _process(delta):
 	
 	if Input.is_action_pressed("shoot"):
@@ -32,3 +47,13 @@ func _process(delta):
 	match plunger_state:
 		State.DEFAULT:
 			world_plunger.global_transform = plunger_end.global_transform
+		State.SHOOT:
+			# if it's been seconds_until_return seconds, return the plunger
+			if world_plunger.global_position.distance_to(plunger_end.global_position) > max_distance_from_player:
+				_return_plunger()
+		State.RETURNING:
+			if world_plunger.global_position.distance_to(plunger_end.global_position) < 1:
+				plunger_state = State.DEFAULT
+				world_plunger.global_transform = plunger_end.global_transform
+
+				
