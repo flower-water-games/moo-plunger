9d989709cd147a1c8e9d4f085ca9a51fe99441a2
squiffel <qytiswork@gmail.com>
2024-01-20 22:05:29 -0500

    Plunger

--- Entities/PlungerGun/PlungerGun.gd
+++ Entities/PlungerGun/PlungerGun.gd
@@ -5,28 +5,30 @@ var plunger_scene = load("res://Entities/Plunger/Plunger.tscn")
 @onready var plunger_end : Node3D = $PlungerEnd
 var world_plunger : CharacterBody3D
 
-var action_shoot : bool = false
-var action_stuck : bool = false
-var action_retrieve : bool = false
+enum State {SHOOT, STUCK, RETURN, DEFAULT}
+var plunger_state = State.DEFAULT
 
 var max_distance_from_player : int = 30
 
 func _ready():
-	# Add the Plunger to Level3D. This version of the Plunger has been shot out of the gun
+	# Add the Plunger to Level3D (Not a child of the Player)
 	world_plunger = plunger_scene.instantiate()
 	world_plunger.global_transform = plunger_end.global_transform
 	get_tree().get_root().add_child(world_plunger)
-	
-	# This Plunger is directly attached to the gun so it will have all the same animations
 
-
-func _input(event):
-	if event is InputEventKey:
-		if Input.is_action_pressed("shoot"):
-			_shoot_plunger()
 
 func _shoot_plunger():
-	print("Fire!")
+	if plunger_state == State.DEFAULT:
+		plunger_state = State.SHOOT
+		world_plunger.speed = 10.0
+		world_plunger.direction = global_position.direction_to(plunger_end.global_position)
+		print("Fire!")
 
 func _process(delta):
-	world_plunger.global_transform = plunger_end.global_transform
+	
+	if Input.is_action_pressed("shoot"):
+		_shoot_plunger()
+		
+	match plunger_state:
+		State.DEFAULT:
+			world_plunger.global_transform = plunger_end.global_transform
