3ee2d3bec0d192b36f92e4c14e8ba473e1528dfe
squiffel <qytiswork@gmail.com>
2024-01-22 23:15:17 -0500

    Basic moles added

--- Entities/Plunger/Plunger.gd
+++ Entities/Plunger/Plunger.gd
@@ -6,7 +6,7 @@ signal cow_hit(cow)
 var speed : float = 0.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity : float = 9.8 #ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var direction : Vector3 = Vector3.ZERO
 var previous_position : Vector3 = Vector3.ZERO
