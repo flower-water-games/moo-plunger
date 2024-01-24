5cdee980e9ddd96f1a534b47a48885f9070fe0dc
Carlos Michael <carlosmichaeldev@gmail.com>
2024-01-23 21:32:51 -0500

    added bokhum anims and states to moles

--- Entities/CowFarm/farm_manager.gd
+++ Entities/CowFarm/farm_manager.gd
@@ -15,7 +15,7 @@ var player_UI : Label
 func _ready():
 	player_UI = get_node("/root/Level3D/PLAYER/PlayerUI/Label")
 	# in godot 4 set the timer to call add_cow_bonus
-	bonus_timer.connect("timeout", add_cow_bonus)
+	#bonus_timer.connect("timeout", add_cow_bonus)
 
 
 
@@ -23,6 +23,9 @@ var cows_inside = 0
 
 var player_currency = 0
 
+func add_currency(amount):
+	player_currency += amount
+	player_UI.text = make_status_string()
 
 func add_cow_bonus():
 	player_currency += cows_inside * cost_per_cow
@@ -41,4 +44,4 @@ func _on_area_3d_body_exited(body:Node3D):
 		player_UI.text = "oh no cow loose!\n" + make_status_string()
 
 func make_status_string():
-	return "you have " + str(cows_inside) + " cows" + "\n" + "you have " + str(player_currency) + " currency"
+	return "you have " + str(cows_inside) + " cows" + "\n" + "you have " + str(player_currency) + " moles"
--- Entities/Mole/Mole.gd
+++ Entities/Mole/Mole.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+class_name Mole
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -11,11 +12,19 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var cow_group : Node3D
 var my_cow = null
 
+@onready var animator : AnimationPlayer = $GGJ_Mole_Animated3/AnimationPlayer
+var is_being_retrieved = false
+enum State {PICKING, SEEKING_UNDERGROUND, INFLATING, DYING}
+
+var mole_state : State = State.PICKING
+
 func _ready():
 	print("Mole Spawned!")
 	call_deferred("_pick_a_cow")
+	animator.connect("animation_finished", _can_be_animated_again)
 
 func _pick_a_cow():
+	mole_state = State.PICKING
 	var grounded_cow_list : Array = []
 	for cow in cow_group.get_children():
 		# Add cows that are on the floor
@@ -29,6 +38,7 @@ func _pick_a_cow():
 		my_cow = grounded_cow_list[0]
 		# Connect to the cow
 		my_cow.cow_inflated.connect(_cow_has_inflated)
+		mole_state = State.SEEKING_UNDERGROUND
 		
 	else:
 		# Search for another cow after a X second delay
@@ -44,12 +54,37 @@ func _destroy_mole():
 func _inflate_the_cow():
 	if not my_cow == null:
 		my_cow.air_inflation += 1.0
+		mole_state = State.INFLATING
+
+var animated = false
+
+func _process(delta):
+	match mole_state:
+		State.PICKING:
+			print("Picking a cow")
+		State.SEEKING_UNDERGROUND:
+			print("Seeking underground")
+		State.INFLATING:
+			if not animated:
+				animator.play("Pop_out_Ground")
+				animated = true
+
+func _can_be_animated_again():
+	animated = false
+
 
 func _physics_process(delta):
+	if is_being_retrieved:
+		# velocity.x = 0
+		# velocity.z = 0
+		velocity.x = 0
+		velocity.z = 0
+		my_cow = null
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
+	# if 
 	if not my_cow == null:
 		# Move towards cow
 		if global_position.distance_to(my_cow.global_position) > 3.0:
@@ -62,7 +97,7 @@ func _physics_process(delta):
 			velocity.z *= 0.8
 			# Inflate the cow
 			_inflate_the_cow()
-	
+		
 	move_and_slide()
 	
 	if position.y < -40.0:
