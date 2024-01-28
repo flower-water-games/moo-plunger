de012f8d4360c16a8f919de165a5788e865ae581
Carlos Michael <carlosmichaeldev@gmail.com>
2024-01-28 15:29:11 -0500

    added web detection

--- Managers/ButtonGoToLevel/ButtonGoToLevel.gd
+++ Managers/ButtonGoToLevel/ButtonGoToLevel.gd
@@ -3,9 +3,18 @@ extends Node
 @export var button : Button
 @export var level_path : String = ("res://UserInterface/MainMenu/MainMenu.tscn")
 
+@export var web_build : bool = false
 func _ready():
 	button.pressed.connect(_go_to_level)
 
 func _go_to_level():
 	print("go to: " + level_path)
-	get_tree().change_scene_to_file(level_path)
+	if web_build:
+		match OS.get_name():
+			"Web":
+				get_tree().change_scene_to_file("res://Levels/web_lit_level.tscn")
+				print("Web")
+	else:
+		get_tree().change_scene_to_file(level_path)
+		print("Not Web")
+
