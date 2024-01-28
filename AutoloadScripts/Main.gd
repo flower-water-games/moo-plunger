724ed35cec86a9e49017fc5cb07a80f2e37990b3
Carlos Michael <carlosmichaeldev@gmail.com>
2024-01-28 15:25:26 -0500

    changed quit

--- AutoloadScripts/Main.gd
+++ AutoloadScripts/Main.gd
@@ -6,8 +6,8 @@ func _input(event):
 	if event is InputEventKey:
 		if event.is_action_pressed("close_game"):
 			# get_tree().quit()
-			get_tree().change_scene("res://UserInterface/MainMenu/TitleScreen.tscn")
 			game_over()
+			get_tree().change_scene("res://UserInterface/MainMenu/TitleScreen.tscn")
 			pass
 
 func game_over():
